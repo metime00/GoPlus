7e3e84ede43369a22388bb64452a97b4cf735449
metime00 <metime00@gmail.com>
2016-03-10 19:49:21 -0500

    added decoding, made tests to ensure encoding and decoding works

--- GoPlus/Network.fs
+++ GoPlus/Network.fs
@@ -13,35 +13,36 @@ open System.Net.Sockets
 let pieceToBytes (color, shape) =
     [|
         match color with
-        | Color.Black -> yield byte 0
-        | Color.White -> yield byte 1
-        | Color.Neutral -> yield byte 2
+        | Color.Black -> yield 0uy
+        | Color.White -> yield 1uy
+        | Color.Neutral -> yield 2uy
         | _ -> failwith "should not be adding a powerup piece this way"
 
         match shape with
-        | Shape.Normal -> yield byte 0
-        | Shape.L -> yield byte 1
+        | Shape.Normal -> yield 0uy
+        | Shape.L -> yield 1uy
         | Shape.Big (xSize, ySize) ->
-            yield byte 2
+            yield 2uy
             yield! BitConverter.GetBytes xSize
             yield! BitConverter.GetBytes ySize
     |]
 
-let moveToByes move =
+/// Encodes a move as bytes
+let moveToBytes move =
     [|
         match move with
             | AddPiece (piece, (x, y)) ->
                         
-                yield byte 0
+                yield 0uy
                 yield! pieceToBytes piece
                 yield! BitConverter.GetBytes x
                 yield! BitConverter.GetBytes y
             | RemovePiece (x, y) ->
-                yield byte 1
+                yield 1uy
                 yield! BitConverter.GetBytes x
                 yield! BitConverter.GetBytes y
             | MarkDead pieces ->
-                yield byte 2
+                yield 2uy
                 yield! BitConverter.GetBytes (List.length pieces)
                 yield!
                     [|
@@ -50,29 +51,23 @@ let moveToByes move =
                             yield! BitConverter.GetBytes y
                     |]
             | Pass ->
-                yield byte 3
+                yield 3uy
             | Move.Conway ->
-                yield byte 4
+                yield 4uy
             | Move.Shuffle percent ->
-                yield byte 5
+                yield 5uy
                 yield! BitConverter.GetBytes percent
     |]
 
 /// encodes a list of moves as an array of bytes for sending to the other player
-let encode moves =
-    let moveBytes =
-        [|
-            for i in moves do
-                yield! i
-        |]
-
+let encode (moves : Move list) =
     [|
-        yield! BitConverter.GetBytes (Array.length moveBytes)
-        yield! moveBytes
+        for i in moves do
+            yield! moveToBytes i
     |]
    
 ///decodes a piece and returns a tuple of bytesRead * piece
-let decodePiece (bytes : byte []) startIndex =
+let decodePiece startIndex (bytes : byte []) =
     let col =
         match bytes.[startIndex] with
         | 0uy -> Color.Black
@@ -94,9 +89,47 @@ let decodePiece (bytes : byte []) startIndex =
         | _ -> 2
     (bytesRead, (col, shape))
 
+///decodes a move and returns a tuple of bytesRead * move
+let decodeMove startIndex (bytes : byte []) = 
+    match bytes.[startIndex] with
+    | 0uy -> //addPiece
+        let (pieceOffset, piece) = decodePiece (startIndex + 1) bytes
+        let x = BitConverter.ToInt32 (bytes, startIndex + 1 + pieceOffset)
+        let y = BitConverter.ToInt32 (bytes, startIndex + 5 + pieceOffset)
+        (9 + pieceOffset, Move.AddPiece (piece, (x, y)))
+    | 1uy -> //removePiece
+        let x = BitConverter.ToInt32 (bytes, startIndex + 1)
+        let y = BitConverter.ToInt32 (bytes, startIndex + 5)
+        (9, Move.RemovePiece (x, y))
+    | 2uy -> //markDead
+        let length = BitConverter.ToInt32 (bytes, startIndex + 1)
+        let rec convertCoords curList offset =
+            if List.length curList < length then
+                let x = BitConverter.ToInt32 (bytes, offset)
+                let y = BitConverter.ToInt32 (bytes, offset + 4)
+                convertCoords ((x, y) :: curList) (offset + 8)
+            else
+                List.rev curList
+        let deadPieces = convertCoords [] (startIndex + 5)
+        (2 + 8 * (List.length deadPieces), Move.MarkDead deadPieces)
+    | 3uy -> //pass
+        (1, Move.Pass)
+    | 4uy -> //conway
+        (1, Move.Conway)
+    | 5uy -> //shuffle
+        let percent = BitConverter.ToInt32 (bytes, startIndex + 1)
+        (5, Move.Shuffle percent)
+    | _ -> failwith "invalid byte for matching a shape to"
+        
 /// Takes a message and returns a list of moves that is the other player's turn
 let decode signal =
-    ()
+    let rec decode curMoves pos =
+        if pos < Array.length signal then
+            let (offset, nextMove) = decodeMove pos signal
+            decode (nextMove :: curMoves) (pos + offset)
+        else
+            List.rev curMoves
+    decode [] 0
 
 type SignalArgs (message) =
     member this.Message = message
---
+++ GoPlusTests/Tests.fs
@@ -0,0 +1,94 @@
+﻿namespace GoPlusTests
+// TODO
+// 1. make all tests be situations that could really happen in the game
+
+
+open System
+open Network
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Pieces
+open GameOptions
+open Game
+open Gameplay
+open System
+open Board
+
+[<TestClass>]
+type BoardActions() = 
+    [<TestMethod>]
+    member this.PowerupNotObstruct () = 
+        let size = 3
+        let game = new Game (size, { NeutralGen = false }, Guaranteed, 0)
+        game.Board.[0,0] <- Some (Pickup (Powerup.L), Shape.Normal)
+        game.Board.[0,1] <- Some (Black, Shape.Normal)
+        game.Board.[0,2] <- Some (White, Shape.Normal)
+        game.Board.[2,0] <- Some (Pickup (Powerup.L), Shape.Normal)
+        game.Board.[2,2] <- Some (Pickup (Powerup.L), Shape.Normal)
+        Assert.AreEqual (game.MakeMoves [ (1, 1) ] , Accept ())
+    
+    [<TestMethod>]
+    member this.IdenticalNeutralGeneration () =
+        let size = 9
+        let game1 = new Game (size, { NeutralGen = true }, Guaranteed, 0)
+        let game2 = new Game (size, { NeutralGen = true }, Guaranteed, 0)
+        
+        for i = 0 to size - 1 do
+            for j = 0 to size - 1 do
+                Assert.AreEqual (game1.Board.[i,j], game2.Board.[i,j])
+
+    [<TestMethod>]
+    member this.IdenticalPowerupPlacement () =
+        let size = 3
+        let game1 = new Game (size, { NeutralGen = false }, Guaranteed, 0)
+        let game2 = new Game (size, { NeutralGen = false }, Guaranteed, 0)
+        
+        Assert.AreEqual (game1.MakeMoves [ (1, 1) ] , Accept ())
+        Assert.AreEqual (game2.MakeMoves [ (1, 1) ] , Accept ())
+
+        for i = 0 to size - 1 do
+            for j = 0 to size - 1 do
+                Assert.AreEqual (game1.Board.[i,j], game2.Board.[i,j])
+
+[<TestClass>]
+type EncodingDecoding() = 
+    [<TestMethod>]
+    member this.EncodingFullMove () = 
+        let moves = [ Move.AddPiece ((Black, Normal), (5, 7)); Move.AddPiece ((Black, Normal), (2, 9)); Move.AddPiece ((Black, Normal), (0, 0)) ]
+        let encodeDecode = moves |> encode |> decode
+        Assert.AreEqual(moves, encodeDecode)
+    
+    [<TestMethod>]
+    member this.EncodingAddNormalPiece () = 
+        let move = Move.AddPiece ((Black, Normal), (5, 7))
+        let (_, encodeDecode) = move |> moveToBytes |> decodeMove 0
+        Assert.AreEqual(move, encodeDecode)
+
+    [<TestMethod>]
+    member this.EncodingAddBigPiece () = 
+        let move = Move.AddPiece ((White, Big (1, 1)), (5, 7))
+        let (_, encodeDecode) = move |> moveToBytes |> decodeMove 0
+        Assert.AreEqual(move, encodeDecode)
+
+    [<TestMethod>]
+    member this.EncodingRemovePiece () = 
+        let move = Move.RemovePiece (0, 15)
+        let (_, encodeDecode) = move |> moveToBytes |> decodeMove 0
+        Assert.AreEqual(move, encodeDecode)
+
+    [<TestMethod>]
+    member this.EncodingPass () = 
+        let move = Move.Pass
+        let (_, encodeDecode) = move |> moveToBytes |> decodeMove 0
+        Assert.AreEqual(move, encodeDecode)
+
+    [<TestMethod>]
+    member this.EncodingMarkDead () = 
+        let move = Move.MarkDead [ (0,0); (5, 7); (2, 3); (3, 2) ]
+        let (_, encodeDecode) = move |> moveToBytes |> decodeMove 0
+        Assert.AreEqual(move, encodeDecode)
+
+    [<TestMethod>]
+    member this.EncodingPiece () = 
+        let piece = (Black, Normal)
+        let (_, encodeDecode) = piece |> pieceToBytes |> decodePiece 0
+        Assert.AreEqual(piece, encodeDecode)
